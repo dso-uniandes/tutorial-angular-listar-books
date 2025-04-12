e61fea58300c8982958ab49e1fd3b221e7a20637
Daniel Serna Osorio <d.sernao@uniandes.edu.co>
2025-04-12 10:26:24 -0500

    Definir el Modelo del componente

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../book';
 
 @Component({
   selector: 'app-book-list',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookListComponent implements OnInit {
 
+  books: Array<Book> = [];
   constructor() { }
 
   ngOnInit() {
